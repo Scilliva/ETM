0d833b34da7cd2e58254323f7e807c87d9075485
Val S <valolympique@gmail.com>
2025-05-27 12:33:06 +0200

    Updates

--- static/js/53.84b8515f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpETMGame=this.webpackJsonpETMGame||[]).push([[53],{222:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(27),o=e(26),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.84b8515f.chunk.js.map
